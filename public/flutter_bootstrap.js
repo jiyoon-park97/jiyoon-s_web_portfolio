1295ceb8216222e6f59896d9c8a83a0e6daf08f7
jiyoon-park97 <pjy3065081@naver.com>
2025-12-26 22:25:28 +0900

    update

--- public/flutter_bootstrap.js
+++ public/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3708788712"
+    serviceWorkerVersion: "1423234530"
   }
 });
