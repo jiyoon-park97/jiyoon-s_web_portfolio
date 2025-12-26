be6bbcf9446aa90e5a0c6f5ef88d40313a907880
jiyoon-park97 <pjy3065081@naver.com>
2025-12-26 22:28:24 +0900

    update

--- lib/contact.dart
+++ lib/contact.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/gestures.dart';
-import 'package:url_launcher/url_launcher.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
